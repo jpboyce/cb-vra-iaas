e88c5ff6eac3081df3fae3f497a63741ecb4ad45
Jesse Boyce <jesse@jpboyce.org>
2018-09-28 07:02:58 +1000

    Formatting

--- metadata.rb
+++ metadata.rb
@@ -1,26 +1,16 @@
-name 'cb_vra_iaas'
-maintainer 'Jesse Boyce'
-maintainer_email 'jesse@jpboyce.org'
-license 'All Rights Reserved'
-description 'Installs/Configures cb_vra_iaas'
-long_description 'Installs/Configures cb_vra_iaas'
-version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+name              'cb_vra_iaas'
+maintainer        'Jesse Boyce'
+maintainer_email  'jesse@jpboyce.org'
+license           'MIT'
+description       'Installs/Configures cb_vra_iaas'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
+supports          'windows'
+depends           'windows'
+source_url        'https://github.com/jpboyce/cb_vra_iaas'
+issues_url        'https://github.com/jpboyce/cb_vra_iaas/issues'
+chef_version      '>= 12.1' if respond_to?(:chef_version)
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-issues_url 'https://github.com/jpboyce/cb_vra_iaas/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-source_url 'https://github.com/jpboyce/cb_vra_iaas'
-
-supports 'windows'
-depends 'windows'
 # Added for java install
 # depends 'java', '~> 2.1.0'
 # Added for MSSQL install
