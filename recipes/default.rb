550eb3a58dcdb0c769d367dddadb6b60d1499073
Jesse Boyce <jesse@jpboyce.org>
2018-09-29 05:33:19 +1000

    Testing appveyor with blank default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-include_recipe 'cb_vra_iaas::iis'
+# include_recipe 'cb_vra_iaas::filebeat'
 # include_recipe 'cb_vra_iaas::iis'
 # include_recipe 'cb_vra_iaas::java'
 # include_recipe 'cb_vra_iaas::dtc_service'
