75e6ff14677a0b77729d3db1e93926cf69c8267a
Jesse Boyce <jesse@jpboyce.org>
2018-09-29 11:13:08 +1000

    Commented out spec tests

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,17 +6,17 @@
 
 require 'spec_helper'
 
-describe 'cb-vra-iaas::default' do
-  context 'When all attributes are default, on Windows 2012' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2016')
-      runner.converge(described_recipe)
-    end
+# describe 'cb-vra-iaas::default' do
+#  context 'When all attributes are default, on Windows 2012' do
+#    let(:chef_run) do
+#      # for a complete list of available platforms and versions see:
+#      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+#      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2016')
+#      runner.converge(described_recipe)
+#    end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-end
+#    it 'converges successfully' do
+#      expect { chef_run }.to_not raise_error
+#    end
+#  end
+# end
