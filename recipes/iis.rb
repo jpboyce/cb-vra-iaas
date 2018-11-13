d6819cebc367a223fbe7dde18a9048f20c6b4de9
Jesse Boyce <jesse@jpboyce.org>
2018-11-14 01:04:00 +1000

    Added Disable Anonymous Access resource

--- recipes/iis.rb
+++ recipes/iis.rb
@@ -7,10 +7,19 @@
 require 'chef/win32/version'
 windows_version = Chef::ReservedNames::Win32::Version.new
 
-if windows_version.windows_server_2012?
-  # Install the IIS roles as defined for Windows 2016
-  windows_feature_powershell node['cb_vra_iaas']['iis_features']['2016'] do
-    feature_name node['cb_vra_iaas']['iis_features']['2016']
+if windows_version.windows_server_2012_r2?
+  # Install the IIS roles as defined for Windows 2012R2
+  windows_feature_powershell node['cb_vra_iaas']['iis_features']['2012r2'] do
+    feature_name node['cb_vra_iaas']['iis_features']['2012r2']
     action :install
   end
+end
+
+# Disable Anonymous Authentication
+powershell_script 'DisableAnonymousAuthentication' do
+  code <<-EOH
+  Set-WebConfigurationProperty -Filter /system.webServer/security/authentication/anonymousAuthentication -Name enabled -Value $false  -PSPath 'IIS:\'
+  EOH
+  guard_interpreter :powershell_script
+  only_if '(Get-WebConfigurationProperty -filter /system.webServer/security/authentication/anonymousAuthentication -name enabled -PSPath "IIS:\" ).Value'
 end
