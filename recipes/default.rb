85663c357a9d98b2be2527ecb3858c080ab28b02
Jesse Boyce <jesse@jpboyce.org>
2019-01-08 05:27:56 +1000

    misc updates

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,6 +5,9 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 # include_recipe 'cb_vra_iaas::filebeat'
-# include_recipe 'cb_vra_iaas::iis'
-# include_recipe 'cb_vra_iaas::java'
+include_recipe 'cb_vra_iaas::iis'
+
+# Working recipes
 # include_recipe 'cb_vra_iaas::dtc_service'
+# include_recipe 'cb_vra_iaas::java'
+# include_recipe 'cb_vra_iaas::dotnet'
