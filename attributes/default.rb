7ec1fd0b5ffbe33fc57c9ee2762605568be36ab3
Jesse Boyce <jesse@jpboyce.org>
2019-01-08 05:27:43 +1000

    misc updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,7 @@
 # Default attributes
 
+default['cb_vra_iaas']['asset_root'] = 'https://s3-ap-southeast-2.amazonaws.com/jpboycechefassets/'
+
 # IAAS Server Roles to install
 default['cb_vra_iaas']['server_role']['web'] = true
 default['cb_vra_iaas']['server_role']['manager'] = true
@@ -9,7 +11,8 @@ default['cb_vra_iaas']['server_role']['sql'] = true
 default['cb_vra_iaas']['iaas_svc_account']['name'] = ''
 default['cb_vra_iaas']['iaas_svc_account']['password'] = ''
 # IIS Features List
-default['cb_vra_iaas']['iis_features']['2016'] = %w(Web-Windows-Auth Web-Static-Content Web-Default-Doc Web-Asp-Net Web-Asp-Net45 Web-ISAPI-Ext Web-ISAPI-Filter WAS-Config-APIs WAS-NET-Environment WAS-Process-Model NET-WCF-HTTP-Activation45 NET-HTTP-Activation NET-Non-HTTP-Activ )
+default['cb_vra_iaas']['iis_features']['2016'] = %w(Web-Windows-Auth Web-Static-Content Web-Default-Doc Web-Asp-Net Web-Asp-Net45 Web-ISAPI-Ext Web-ISAPI-Filter WAS-Config-APIs WAS-NET-Environment WAS-Process-Model NET-WCF-HTTP-Activation45 NET-HTTP-Activation NET-Non-HTTP-Activ Web-Mgmt-Tools )
+default['cb_vra_iaas']['iis_features']['2012r2'] = %w(Web-Windows-Auth Web-Static-Content Web-Default-Doc Web-Asp-Net Web-Asp-Net45 Web-ISAPI-Ext Web-ISAPI-Filter WAS-Config-APIs WAS-NET-Environment WAS-Process-Model NET-WCF-HTTP-Activation45 NET-HTTP-Activation NET-Non-HTTP-Activ Web-Mgmt-Tools )
 
 # Filebeat Installer attributes
 default['cb_vra_iaas']['filebeat']['name'] = 'filebeat-6.3.2-windows-x86_64.zip'
@@ -26,14 +29,14 @@ default['cb_vra_iaas']['java']['name'] = 'Java 8 Update 191 (64-bit)'
 default['cb_vra_iaas']['java']['installer'] = 'jre-8u191-windows-x64.exe'
 default['cb_vra_iaas']['java']['checksum'] = '605d05442c1640530a8ca2938baafb785560aefa88dc8cd0b43261ef3ecfa4bd'
 default['cb_vra_iaas']['java']['version'] = '8.0.1910.12'
-default['cb_vra_iaas']['java']['source'] = 'jre-8u191-windows-x64.exe.zip'
+default['cb_vra_iaas']['java']['source'] = "#{node['cb_vra_iaas']['asset_root']}jre-8u191-windows-x64.exe"
 
 # .NET 4.5.2 Installer
 default['cb_vra_iaas']['dotnet']['name'] = 'Microsoft .NET Framework 4.5.2'
 default['cb_vra_iaas']['dotnet']['installer'] = 'NDP452-KB2901907-x86-x64-AllOS-ENU.exe'
 default['cb_vra_iaas']['dotnet']['checksum'] = '6c2c589132e830a185c5f40f82042bee3022e721a216680bd9b3995ba86f3781'
 default['cb_vra_iaas']['dotnet']['version'] = '4.5.51650'
-default['cb_vra_iaas']['dotnet']['source'] = 'https://download.microsoft.com/download/E/2/1/E21644B5-2DF2-47C2-91BD-63C560427900/NDP452-KB2901907-x86-x64-AllOS-ENU.exe'
+default['cb_vra_iaas']['dotnet']['source'] = "#{node['cb_vra_iaas']['asset_root']}NDP452-KB2901907-x86-x64-AllOS-ENU.exe"
 
 # SQL Installer
 # default['cb_vra_iaas']['sql2012']['name'] = ''
